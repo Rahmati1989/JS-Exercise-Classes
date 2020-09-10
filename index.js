6cc5f7f34caf4adef97820152cdd3c838ad7c8f5
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-09-09 18:45:25 -0700

    Completed task1 and task2

--- index.js
+++ index.js
@@ -41,8 +41,32 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  
+  eat(food){ 
+    if(this.stomach.length < 10){
+      this.stomach.push(food);}
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+   return `${this.name} is ${this.age} years old!`;
+  }
 
 }
+const Andrew = new Person("Andrew", 27);
+
+console.log(Andrew);
+Andrew.eat("chicken");
+console.log(Andrew.stomach);
+console.log(Andrew.toString())
 
 /*
   TASK 2
@@ -59,8 +83,35 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    if (this.tank - (distance/this.milesPerGallon) < 0){
+      this.odometer = this.odometer + this.milesPerGallon * this.tank;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+   }
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance/this.milesPerGallon);
+  }
 }
+
+const Ford = new Car("Ford", 20);
+
+console.log(Ford);
+Ford.fill(20);
+console.log(Ford.tank);
+Ford.drive(400);
+console.log(Ford.odometer);
+console.log(Ford.tank);
+
 
 /*
   TASK 3
